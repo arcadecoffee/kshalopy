a63c0cf52b000b7bb8f5702e4d4721decedb7a56
Jerry Gregoire <jerry@arcadecoffee.com>
2022-02-16 15:56:59 -0600

    screwing around

--- auth_driver.py
+++ auth_driver.py
@@ -4,8 +4,10 @@ Test harness for logins
 
 import logging
 import os
+import signal
 import threading
-import time
+
+from datetime import datetime
 
 from kshalopy import Config, Credentials
 from kshalopy.login import LoginHandler, LoginParameters, VerificationMethods
@@ -17,12 +19,21 @@ CREDENTIALS_FILE = 'credentials.secret.json'
 config = Config.load_defaults()
 
 
+def refresh_credentials(credentials: Credentials, exit_event, offset: int = 600):
+    while True:
+        if credentials.ttl < offset:
+            logging.info("Getting fresh credentials")
+            credentials.get_fresh_tokens()
+            credentials.save_credentials(CREDENTIALS_FILE)
+            logging.info("Got fresh credentials")
+        if exit_event.wait(timeout=(credentials.ttl - offset)):
+            break
+    logging.info("Exiting")
+
+
 def main():
     if os.path.exists(CREDENTIALS_FILE):
         credentials = Credentials.load_credentials(CREDENTIALS_FILE)
-        if credentials.ttl < 300:
-            credentials.get_fresh_tokens()
-            credentials.save_credentials(CREDENTIALS_FILE)
     else:
         login_params = LoginParameters(
             username=input("Username: "),
@@ -42,6 +53,22 @@ def main():
 
         credentials = authenticator.credentials
         credentials.save_credentials(CREDENTIALS_FILE)
+
+    exit_event = threading.Event()
+
+    signal.signal(signal.SIGINT, lambda a, b: exit_event.set())
+
+    worker = threading.Thread(
+        target=refresh_credentials, args=[credentials, exit_event]
+    )
+    worker.start()
+
+    while (not worker.join(1)) and worker.is_alive():
+        print(
+            f"\r{round(credentials.ttl)} : "
+            f"{datetime.fromtimestamp(credentials.expiration).isoformat()}",
+            end=""
+        )
 
 
 if __name__ == "__main__":
